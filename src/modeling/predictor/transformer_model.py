337ee166f792934ceeecc6487f24f0eee759b1d0
Chris Zhang <zhanghaolin66@gmail.com>
2025-06-19 17:53:54 +0000

    Fix dtype missmatch

--- src/modeling/predictor/transformer_model.py
+++ src/modeling/predictor/transformer_model.py
@@ -163,7 +163,7 @@ class TransformerNetModel(nn.Module):
 
         # https://github.com/huggingface/transformers/blob/e95d433d77727a9babadf008dd621a2326d37303/src/transformers/modeling_utils.py#L700
         if attention_mask is not None:
-            attention_mask = attention_mask[:, None, None, :]
+            attention_mask = attention_mask[:, None, None, :].to(dtype=torch.bool)
 
         input_trans_hidden_states = self.input_transformers(
             emb_inputs, attention_mask=attention_mask
